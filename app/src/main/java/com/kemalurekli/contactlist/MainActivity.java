8c55817367cffefd48e206f47004745e0e0b08ae
Kemal <>
2021-01-30 12:16:06 +0300

    Layout Created

--- app/src/main/java/com/kemalurekli/contactlist/MainActivity.java
+++ app/src/main/java/com/kemalurekli/contactlist/MainActivity.java
@@ -1,14 +1,21 @@
 package com.kemalurekli.contactlist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+
+    RecyclerView recyclerView;
+    ArrayList<ContactModel> arrayList = new ArrayList<ContactModel>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recylerView);
     }
 }
